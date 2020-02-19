2771b20d2fa63e00af3db5c4d849526ddc735cb9
Toxa <kuzmin0609@gmail.com>
2020-02-19 20:35:10 +0200

    added DeliveryRepository, DeliveryRepositoryTests

---
+++ src/DataAccess/Repository/Interfaces/DeliveryRepository.cs
@@ -0,0 +1,46 @@
+﻿using Domain.Context;
+using Domain.EF_Models;
+using Domain.Infrastructure;
+using Domain.Repository.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Repository.Interfaces
+{
+    public class DeliveryRepository : BaseRepository<Delivery>, IDeliveryRepository
+    {
+        public DeliveryRepository(StoreContext context) : base(context)
+        {
+
+        }
+
+        public OperationDetail CreateComment(Delivery delivery)
+        {
+            return Create(delivery);
+        }
+
+        public OperationDetail DeleteComment(Delivery delivery)
+        {
+            return Delete(delivery);
+        }
+
+        public async Task<IEnumerable<Delivery>> FindAllCommentsAsync()
+        {
+            return await FindAll().ToListAsync();
+        }
+
+        public async Task<IEnumerable<Delivery>> FindCommentByConditionAsync(Expression<Func<Delivery, bool>> predicate)
+        {
+            return await FindByCondition(predicate).ToListAsync();
+        }
+
+        public OperationDetail UpdateComment(Delivery delivery)
+        {
+            return Update(delivery);
+        }
+    }
+}

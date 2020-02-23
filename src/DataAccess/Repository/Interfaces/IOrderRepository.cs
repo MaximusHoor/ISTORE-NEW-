a176c2e5377060007068fb272e67927fc39acb4a
MaximusHoor <60979423+MaximusHoor@users.noreply.github.com>
2020-02-23 18:17:44 +0300

    Merge All. Add OrderDetails

---
+++ src/DataAccess/Repository/Interfaces/IOrderRepository.cs
@@ -0,0 +1,22 @@
+﻿using Domain.EF_Models;
+using Domain.Infrastructure;
+using Domain.Repository.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Repository.Interfaces
+{
+    public interface IOrderRepository : IRepository<Order>
+    {
+        Task<IEnumerable<Order>> FindAllOrdersAsync();
+        Task<IEnumerable<Order>> FindOrdersByConditionAsync(Expression<Func<Order, bool>> predicate);
+        Task<User> GetUserByOrderAsync(Order order);
+        Task<Delivery> GetDeliveryByOrderAsync(Order order);
+        Task<ICollection<OrderDetails>> GetProductsByOrderAsync(Order order);
+        Task<OperationDetail> AddOrderDetailsToOrderAsync(Order order, OrderDetails orderDetails);
+        Task<OperationDetail> RemoveOrderDetailsOfOrderAsync(Order order, OrderDetails orderDetails);
+    }
+}
---
+++ src/DataAccess/Repository/OrderRepository.cs
@@ -0,0 +1,66 @@
+﻿using DataAccess.Repository.Interfaces;
+using Domain.Context;
+using Domain.EF_Models;
+using Domain.Infrastructure;
+using Domain.Repository.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using Serilog;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Repository
+{
+    public class OrderRepository : BaseRepository<Order>, IOrderRepository
+    {
+        public OrderRepository(StoreContext context) : base(context)
+        {
+        }
+
+        public async Task<IEnumerable<Order>> FindAllOrdersAsync()
+        {
+            Log.Information($"{DateTime.Now} - FindAllOrdersAsync");
+            return await FindAll().ToListAsync(); throw new NotImplementedException();
+        }
+
+        public async Task<IEnumerable<Order>> FindOrdersByConditionAsync(Expression<Func<Order, bool>> predicate)
+        {
+            Log.Information($"{DateTime.Now} - FindOrdersByConditionAsync");
+            return await FindByCondition(predicate).ToListAsync();
+        }
+
+        public async Task<Delivery> GetDeliveryByOrderAsync(Order order)
+        {
+            Log.Information($"{DateTime.Now} - GetDeliveryByOrderAsync");
+            return (await FindByCondition(x => x.Id == order.Id).FirstOrDefaultAsync()).Delivery;
+        }
+
+        public async Task<ICollection<OrderDetails>> GetProductsByOrderAsync(Order order)
+        {
+            Log.Information($"{DateTime.Now} - GetDeliveryByOrderAsync");
+            return (await FindByCondition(x => x.Id == order.Id).FirstOrDefaultAsync()).Products;
+        }
+
+        public async Task<User> GetUserByOrderAsync(Order order)
+        {
+            Log.Information($"{DateTime.Now} - GetDeliveryByOrderAsync");
+            return (await FindByCondition(x => x.Id == order.Id).FirstOrDefaultAsync()).User;
+        }
+
+        public async Task<OperationDetail> AddOrderDetailsToOrderAsync(Order order, OrderDetails orderDetails)
+        {
+            Log.Information($"{DateTime.Now} - AddOrderDetailsToOrderAsync");
+            (await FindByCondition(x => x.Id == order.Id).FirstOrDefaultAsync()).Products.Add(orderDetails);
+            return new OperationDetail() { IsError = false, Message = "AddOrderDetailsToOrderAsync is OK" };
+        }
+
+        public async Task<OperationDetail> RemoveOrderDetailsOfOrderAsync(Order order, OrderDetails orderDetails)
+        {
+            Log.Information($"{DateTime.Now} - GetDeliveryByOrderAsync");
+            (await FindByCondition(x => x.Id == order.Id).FirstOrDefaultAsync()).Products.Remove(orderDetails);
+            return new OperationDetail() { IsError = false, Message = "RemoveOrderDetailsOfOrderAsync is OK" };
+        }
+    }
+}

07dbc2363782ac57e9c18fb6de5d47488473fc56
Tsypchenko Yulia <yutsypa@icloud.com>
2020-02-23 18:49:58 +0200

    deleted DeliveryService

--- src/Domain/EF_Models/DeliveryService.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Domain.EF_Models
-{
-    public class DeliveryService
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public string Type { get; set; }
-    }
-}

602d7dc907ec1ac6012c131ad37c96c9f335c167
AndrewKissenko <52412068+AndrewKissenko@users.noreply.github.com>
2020-02-26 20:01:55 +0200

    User, comment repos/serv created. Repos covered with U tests

--- tests/DataAccessTest/Repository/CommentRepositoryTest.cs
+++ tests/DataAccessTest/Repository/CommentRepositoryTest.cs
@@ -1,132 +1,126 @@
-﻿//using DataAccess.Repository;
-//using DataAccessTest.Repository.Factory;
-//using Domain.EF_Models;
-//using Domain.Repository.Interfaces;
-//using NUnit.Framework;
-//using System;
-//using System.Collections.Generic;
-//using System.Linq;
+﻿using DataAccess.Repository;
+using DataAccess.Repository.Interfaces;
+using DataAccessTest.Repository.Factory;
+using Domain.EF_Models;
+using Domain.Repository.Interfaces;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
-//namespace DomainTest.Repository
-//{
-//    [TestFixture]
-//    public class CommentRepositoryTest
-//    {
-//        [SetUp]
-//        public void Setup()
-//        {
-//            InitialiseParameters();
-//            _repository = RepositoryFactory.Instance<CommentRepository>();
-//        }
+namespace DomainTest.Repository
+{
+    [TestFixture]
+    public class CommentRepositoryTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+            InitialiseParameters();
+            _repository = RepositoryFactory.Instance<CommentRepository>();
+        }
 
-//        private int _id;
-//        private DateTime _date;
-//        private string _text;
-//        private int? _userId;
-//        private int _productId;
-//        private int _like;
-//        private int _dislike;
-//        private IRepository<Comment> _repository;
+        private int _id;
+        private DateTime _date;
+        private string _text;
+        private int? _userId;
+        private int _productId;
+        private int _like;
+        private int _dislike;
+        private ICommentRepository _repository;
 
-//        private void InitialiseParameters()
-//        {
-//            _id = new Random().Next(0, int.MaxValue);
-//            _date = DateTime.Now;
-//            _text = "Comment Text";
-//            _userId = 1;
-//            _productId = 1;
-//            _like = new Random().Next(0, int.MaxValue);
-//            _dislike = new Random().Next(0, int.MaxValue);
-//        }
+        private void InitialiseParameters()
+        {
+            _id = new Random().Next(0, int.MaxValue);
+            _date = DateTime.Now;
+            _text = "Comment Text";
+            _userId = 1;
+            _productId = 1;
+            _like = new Random().Next(0, int.MaxValue);
+            _dislike = new Random().Next(0, int.MaxValue);
+        }
 
-//        private (int, int, int?) Create()
-//        {
-//            // Arrange
-//            var comment = new Comment
-//            {
-//                Date = _date,
-//                Id = _id,
-//                Text = _text,
-//                Product = new Product { Id = _id },
-//                User = new User { Id = _userId.Value },
-//                Like = _like,
-//                Dislike = _dislike,
-//                UserId = _userId
-//            };
-//            // Act
-//            _repository.Create(comment);
-//            ContextSingleton.GetDatabaseContext().SaveChanges();
+        private async Task<(int, int, int)> CreateAsync()
+        {
+            // Arrange
+            var comment = new Comment
+            {
+                Date = _date,
+                Id = _id,
+                Text = _text,
+                Product = new Product { Id = _id },
+                User = new User { Id = _userId.Value },
+                Like = _like,
+                Dislike = _dislike,
+                UserId = _userId.Value
+            };
+            // Act
+            await _repository.CreateAsync(comment);
+            await ContextSingleton.GetDatabaseContext().SaveChangesAsync();
 
-//            // Assert
-//            Assert.AreNotEqual(0, comment.Id, "Creating new record does not return id");
+            // Assert
+            Assert.AreNotEqual(0, comment.Id, "Creating new record does not return id");
 
-//            return (comment.Id, comment.ProductId, comment.UserId);
-//        }
+            return (comment.Id, comment.ProductId, comment.UserId.Value);
+        }
 
-//        private void Update(int id)
-//        {
-//            // Arrange
-//            var comment = _repository.FindByCondition(x => x.Id == id).FirstOrDefault();
-//            comment.Like = 50;
-//            comment.Date = _date.AddDays(-40);
-//            comment.Text = "Hello";
+        private async Task GetAllAsync()
+        {
+            // Act
+            IEnumerable<Comment> items = await _repository.GetAllAsync();
+            // Assert
+            Assert.IsTrue(items.Any(), "GetAll returned no items.");
+        }
 
-//            // Act
-//            _repository.Update(comment);
-//            ContextSingleton.GetDatabaseContext().SaveChanges();
+        private async Task GetByIDAsync(int id)
+        {
+            // Act
+            var comment = await _repository.FindByConditionAllIncludedAsync(x => x.Id == id);
+            // Assert
+            Assert.IsNotNull(comment, "GetByID returned null.");
+            Assert.AreEqual(id, comment.ElementAt(0).Id);
+            Assert.AreEqual(_date, comment.ElementAt(0).Date);
+            Assert.AreEqual(_dislike, comment.ElementAt(0).Dislike);
+            Assert.AreEqual(_text, comment.ElementAt(0).Text);
+            Assert.AreEqual(_userId, comment.ElementAt(0).UserId);
+            Assert.AreEqual(_productId, comment.ElementAt(0).ProductId);
+        }
+        private async Task GetByUserIDAsync(int userId)
+        {
+            // Act
+            var comment = await _repository.FindByConditionAsync(x => x.UserId == userId);
+            // Assert
+            Assert.IsNotNull(comment, "GetByUserID returned null.");
+            Assert.AreEqual(_id, comment.ElementAt(0).Id);
+            Assert.AreEqual(_date, comment.ElementAt(0).Date);
+            Assert.AreEqual(_dislike, comment.ElementAt(0).Dislike);
+            Assert.AreEqual(_text, comment.ElementAt(0).Text);
+            Assert.AreEqual(_productId, comment.ElementAt(0).ProductId);
+            Assert.AreEqual(_userId, comment.ElementAt(0).UserId);
+        }
+        private async Task GetByProductIDAsync(int productId)
+        {
+            // Act
+            var comment = await _repository.FindByConditionAsync(x => x.ProductId == productId);
+            // Assert
+            Assert.IsNotNull(comment, "GetByUserID returned null.");
+            Assert.AreEqual(_id, comment.ElementAt(0).Id);
+            Assert.AreEqual(_date, comment.ElementAt(0).Date);
+            Assert.AreEqual(_dislike, comment.ElementAt(0).Dislike);
+            Assert.AreEqual(_text, comment.ElementAt(0).Text);
+            Assert.AreEqual(_productId, comment.ElementAt(0).ProductId);
+            Assert.AreEqual(_userId, comment.ElementAt(0).UserId);
+        }
 
-//            var updatedcomment = _repository.FindByCondition(x => x.Id == id).FirstOrDefault();
-
-//            // Assert
-//            Assert.AreEqual(50, updatedcomment.Like, "Record is not updated.");
-//            Assert.AreEqual(_date.AddDays(-40), updatedcomment.Date, "Record is not updated.");
-//            Assert.AreEqual("Hello", updatedcomment.Text, "Record is not updated.");
-//        }
-
-//        private void GetAll()
-//        {
-//            // Act
-//            IEnumerable<Comment> items = _repository.FindAll().ToList();
-//            // Assert
-//            Assert.IsTrue(items.Any(), "GetAll returned no items.");
-//        }
-
-//        private void GetByID(int id)
-//        {
-//            // Act
-//            var comment = _repository.FindByCondition(x => x.Id == id).FirstOrDefault();
-//            // Assert
-//            Assert.IsNotNull(comment, "GetByID returned null.");
-//            Assert.AreEqual(id, comment.Id);
-//            Assert.AreEqual(_date, comment.Date);
-//            Assert.AreEqual(_dislike, comment.Dislike);
-//            Assert.AreEqual(_text, comment.Text);
-//            Assert.AreEqual(_userId, comment.UserId);
-//            Assert.AreEqual(_productId, comment.ProductId);
-//        }
-
-//        private void Delete(int id)
-//        {
-//            // Arrange
-//            var comment = _repository.FindByCondition(x => x.Id == id).FirstOrDefault();
-//            // Act
-//            _repository.Delete(comment);
-//            ContextSingleton.GetDatabaseContext().SaveChanges();
-//            comment = _repository.FindByCondition(x => x.Id == id).FirstOrDefault();
-//            // Assert
-//            Assert.IsNull(comment, "Record is not deleted.");
-//        }
-
-//        [Test]
-//        public void CommentCrud()
-//        {
-//            var comment = Create();
-//            _productId = comment.Item2;
-//            _userId = comment.Item3;
-//            GetByID(comment.Item1);
-//            GetAll();
-//            Update(comment.Item1);
-//            Delete(comment.Item1);
-//        }
-//    }
-//}
+        [Test]
+        public async Task CommentCrud()
+        {
+            var comment = await CreateAsync();
+            await GetByIDAsync(comment.Item1);
+            await GetByUserIDAsync(comment.Item3);
+            await GetAllAsync();
+            await GetByProductIDAsync(comment.Item2);
+        }
+    }
+}
--- tests/DataAccessTest/Repository/UserRepositoryTest.cs
+++ tests/DataAccessTest/Repository/UserRepositoryTest.cs
@@ -1,127 +1,103 @@
-﻿//using DataAccess.Repository;
-//using DataAccessTest.Repository.Factory;
-//using Domain.EF_Models;
-//using Domain.Repository.Interfaces;
-//using NUnit.Framework;
-//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-
-//namespace DataAccessTest.Repository
-//{
-//    public class UserRepositoryTest
-//    {
-//        [SetUp]
-//        public void Setup()
-//        {
-//            InitialiseParameters();
-//            _repository = RepositoryFactory.Instance<UserRepository>();
-//        }
-
-//        private int _id;
-//        private string _firstName;
-//        private string _lastName;
-//        private int? _addressId;
-//        private string _email;
-//        private string _phoneNumber;
-//        private IRepository<User> _repository;
-//        private void InitialiseParameters()
-//        {
-//            _id = new Random().Next(0, int.MaxValue);
-//            _firstName = "Roma";
-//            _lastName = "Sidorov";
-//            _addressId = 1;
-//            _phoneNumber = "some num";
-//            _email = "some email";
-//        }
-
-//        private (int, int?) Create()
-//        {
-//            // Arrange
-//            var user = new User
-//            {
-//                Id = _id,
-//                FirstName = _firstName,
-//                LastName = _lastName,
-//                Address = new Address { Id = _id },
-//                AddressId = _addressId,
-//                PhoneNumber = _phoneNumber,
-//                Email = _email
-//            };
-//            // Act
-//            _repository.Create(user);
-//            ContextSingleton.GetDatabaseContext().SaveChanges();
-
-//            // Assert
-//            Assert.AreNotEqual(0, user.Id, "Creating new record does not return id");
-
-//            return (user.Id, user.AddressId);
-//        }
-
-//        private void Update(int id)
-//        {
-//            // Arrange
-//            var user = _repository.FindByCondition(x => x.Id == id).FirstOrDefault();
-//            user.Email = "new email";
-//            user.FirstName = "new data";
-//            user.LastName = "new data";
-//            user.PhoneNumber = "new data";
+﻿using DataAccess.Repository;
+using DataAccess.Repository.Interfaces;
+using DataAccessTest.Repository.Factory;
+using Domain.EF_Models;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 
-//            // Act
-//            _repository.Update(user);
-//            ContextSingleton.GetDatabaseContext().SaveChanges();
+namespace DataAccessTest.Repository
+{
+    public class UserRepositoryTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+            InitialiseParameters();
+            _repository = RepositoryFactory.Instance<UserRepository>();
+        }
 
-//            var updatedcomment = _repository.FindByCondition(x => x.Id == id).FirstOrDefault();
+        private int _id;
+        private string _firstName;
+        private string _lastName;
+        private int? _addressId;
+        private string _email;
+        private string _phoneNumber;
+        private int _comment1Id;
+        private int _comment2Id;
+        private DateTime _dateTime1;
+        private IUserRepository _repository;
+        private void InitialiseParameters()
+        {
+            _id = new Random().Next(0, int.MaxValue);
+            _firstName = "Roma";
+            _lastName = "Sidorov";
+            _addressId = 1;
+            _phoneNumber = "some num";
+            _email = "some email";
+            _comment1Id = new Random().Next(0, int.MaxValue);
+            _comment2Id = new Random().Next(0, int.MaxValue);
+            _dateTime1 = new DateTime(1998, 04, 30);
 
-//            // Assert
-//            Assert.AreEqual("new email", updatedcomment.Email, "Record is not updated.");
-//            Assert.AreEqual("new data", updatedcomment.PhoneNumber, "Record is not updated.");
-//        }
+        }
 
-//        private void GetAll()
-//        {
-//            // Act
-//            IEnumerable<User> items = _repository.FindAll().ToList();
-//            // Assert
-//            Assert.IsTrue(items.Any(), "GetAll returned no items.");
-//        }
+        private async Task<(int, int?)> CreateAsync()
+        {
+            // Arrange
+            var user = new User
+            {
+                Id = _id,
+                FirstName = _firstName,
+                LastName = _lastName,
+                Address = new Address { Id = _id },
+                Comments = new List<Comment> { new Comment() { Id = _comment1Id }, new Comment() { Id = _comment2Id } },
+                AddressId = _addressId,
+                PhoneNumber = _phoneNumber,
+                Email = _email
+            };
+            // Act
+            await _repository.CreateAsync(user);
+            ContextSingleton.GetDatabaseContext().SaveChanges();
 
-//        private void GetByID(int id)
-//        {
-//            // Act
-//            var comment = _repository.FindByCondition(x => x.Id == id).FirstOrDefault();
-//            // Assert
-//            Assert.IsNotNull(comment, "GetByID returned null.");
-//            Assert.AreEqual(id, comment.Id);
-//            Assert.AreEqual(_firstName, comment.FirstName);
-//            Assert.AreEqual(_lastName, comment.LastName);
-//            Assert.AreEqual(_email, comment.Email);
-//            Assert.AreEqual(_phoneNumber, comment.PhoneNumber);
-//            Assert.AreEqual(_addressId, comment.AddressId);
-//        }
+            // Assert
+            Assert.AreNotEqual(_id, user.Id, "Creating new record does not return id");
 
-//        private void Delete(int id)
-//        {
-//            // Arrange
-//            var comment = _repository.FindByCondition(x => x.Id == id).FirstOrDefault();
-//            // Act
-//            _repository.Delete(comment);
-//            ContextSingleton.GetDatabaseContext().SaveChanges();
-//            comment = _repository.FindByCondition(x => x.Id == id).FirstOrDefault();
-//            // Assert
-//            Assert.IsNull(comment, "Record is not deleted.");
-//        }
+            return (user.Id, user.AddressId);
+        }
 
-//        [Test]
-//        public void UserCrud()
-//        {
-//            var comment = Create();
-//            _addressId = comment.Item2;
-//            GetByID(comment.Item1);
-//            GetAll();
-//            Update(comment.Item1);
-//            Delete(comment.Item1);
-//        }
-//    }
-//}
+
+
+        private async Task GetAllAsync()
+        {
+            // Act
+            var items = await _repository.FindAllUsersAllIncludedAsync();
+            // Assert
+            Assert.IsTrue(items.Any(), "GetAll returned no items.");
+        }
+
+        private async Task GetByIDAsync(int id)
+        {
+            // Act
+            var comment = await _repository.FindUserByConditionAllIncludedAsync(x => x.Id == id);
+            // Assert
+            Assert.IsNotNull(comment, "GetByID returned null.");
+            Assert.AreEqual(id, comment);
+            Assert.AreEqual(_firstName, comment.ElementAt(0).FirstName);
+            Assert.AreEqual(_lastName, comment.ElementAt(0).LastName);
+            Assert.AreEqual(_email, comment.ElementAt(0).Email);
+            Assert.AreEqual(_phoneNumber, comment.ElementAt(0).PhoneNumber);
+            Assert.AreEqual(_addressId, comment.ElementAt(0).AddressId);
+        }
+
+        [Test]
+        public async Task UserCrud()
+        {
+            var comment = await CreateAsync();
+            await GetByIDAsync(comment.Item1);
+            await GetAllAsync();
+        }
+    }
+}

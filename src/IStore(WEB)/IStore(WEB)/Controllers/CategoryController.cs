3be40ac01fc4439578de3a33de94a716d9c56e0e
Алексей Гриценко <aleshagritsenko@ukr.net>
2020-03-03 21:56:49 +0200

    Add category in main pattern

--- src/IStore(WEB)/IStore(WEB)/Controllers/CategoryController.cs
+++ src/IStore(WEB)/IStore(WEB)/Controllers/CategoryController.cs
@@ -2,12 +2,41 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Business.Service;
 using Microsoft.AspNetCore.Mvc;
 
 namespace IStore_WEB_.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly CategoryService _categoryService;
+
+        public CategoryController(CategoryService categoryService)
+        {
+            this._categoryService = categoryService;
+        }
+        public async Task<IActionResult> ShowCategoryPartial()
+        {
+            var res = await _categoryService.GetAllAsync();
+
+            return View(res.ToList());
+        }
+
+        public async Task<IActionResult> GetCategoryJson()
+        {
+            try
+            {
+                var categoryCollection = await _categoryService.GetAllAsync();
+                var res = Json(categoryCollection);
+                return res;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+        }
         public IActionResult Index()
         {
             return View();

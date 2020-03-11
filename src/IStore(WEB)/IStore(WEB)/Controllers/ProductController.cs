95a3c60dabd77d2de278c80b55e0d5dddc1f0d72
student <student@24-09>
2020-03-11 20:48:07 +0200

    Add View

--- src/IStore(WEB)/IStore(WEB)/Controllers/ProductController.cs
+++ src/IStore(WEB)/IStore(WEB)/Controllers/ProductController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Business.Service;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,9 +9,26 @@ namespace IStore_WEB_.Controllers
 {
     public class ProductController:Controller
     {
+        private readonly ProductCharacteristicService _productCharacteristicService;
+
+        public ProductController(ProductCharacteristicService productCharacteristicService)
+        {
+            this._productCharacteristicService = productCharacteristicService;
+        }
+
+        public async Task<IActionResult> ProductCharacteristicPart(int id)
+        {
+            var res = await _productCharacteristicService.FindByConditionAsync(x=>x.ProductId == id);
+            return View(res.FirstOrDefault());
+        }
+
         public IActionResult Index()
         {
             return View();
         }
     }
+
+
+
+
 }

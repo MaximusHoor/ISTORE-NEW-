c97989ecff61231ed3739457382e3ede35d2107c
MrUsagi <anavisikim1@gmail.com>
2020-03-11 20:47:09 +0200

    Product

--- src/IStore(WEB)/IStore(WEB)/Controllers/ProductController.cs
+++ src/IStore(WEB)/IStore(WEB)/Controllers/ProductController.cs
@@ -1,6 +1,11 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Business.Service;
+using Domain.EF_Models;
+using IStore_WEB_.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,9 +13,51 @@ namespace IStore_WEB_.Controllers
 {
     public class ProductController:Controller
     {
-        public IActionResult Index()
+        private readonly ILogger<ProductController> _logger;
+        private readonly ProductService _productservice;
+
+        public ProductController(ILogger<ProductController> logger, ProductService productservice)
+        {
+            _logger = logger;
+            _productservice = productservice;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var result = await _productservice.GetSortByRatingAsync(24);
+
+            return View(result);
+        }
+
+        public IActionResult Privacy()
         {
             return View();
         }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public async Task<IActionResult> ProductPartial(Product product)
+        {
+            return View(product);
+        }
+
+        public async Task<IActionResult> GetProductDetails(Product product)
+        {
+            var res = await _productservice.GetByIdsync(1);
+
+
+            return PartialView("ProductDetails", res);
+        }
+        public async Task<IActionResult> Product(int id)
+        {
+            var res = await _productservice.GetByIdsync(1);
+
+            return View("Product", res);
+        }
+
     }
 }
